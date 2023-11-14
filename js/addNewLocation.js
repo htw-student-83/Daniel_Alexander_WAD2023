d1ef1d0140ce50a091c43cf644d481662d082a41
student2683@web.de <student2683@web.de>
2023-11-14 16:04:09 +0100

    js for add screen, the only first steps.

--- js/addNewLocation.js
+++ js/addNewLocation.js
@@ -10,11 +10,37 @@ function fromAddToMain(){
 
 //The data, which we get from the user
 function getNewLocationData(){
+    let inputCityName = "";
     let inputLocationName = document.getElementById('name-add').value;
     let inputDescribing = document.getElementById('description-add').value;
     let inputAdress = document.getElementById('address-add').value;
     let inputPC = document.getElementById('postCode-add').value;
-    let inputCityName = document.getElementById('city-add').value;
+    if(document.getElementById('Berlin').value !=="Berlin"){
+        inputCityName = document.getElementById('Berlin').value;
+    }else{
+        inputCityName = document.getElementById('Brandenburg').value;
+    }
     let inputLat = document.getElementById('lat-add').value;
     let inputLon = document.getElementById('lon-add').value;
+
+    if(!inputLocationName == "" || !inputDescribing == "" || !inputAdress == ""){
+        newListItem(inputLocationName, inputDescribing, inputAdress, inputPC, inputCityName, inputLat, inputLon);
+    }else{
+         alert("The fields with * are required.")
+    }
 }
+
+function newListItem(inputLocationName, inputDescribing, inputAdress, inputPC, inputCityName, inputLat, inputLon){
+     var li = document.createElement("li");
+     li.appendChild(document.createTextNode(inputLocationName + ', '
+                                             + inputDescribing + ', '
+                                             + inputAdress +', '
+                                             + inputPC + ', '
+                                             + inputCityName + ', '
+                                             + inputLat + ', '
+                                             + inputLon) + ', ');
+     document.getElementById('table')appendChild(li);
+
+}
+
+
